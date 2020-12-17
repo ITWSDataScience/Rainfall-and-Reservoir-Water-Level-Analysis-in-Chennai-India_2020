6109c71cd638eccf9bbdd59dee06e7191a77cf4b
y1ad7ce <y1ad7ce@gmail.com>
2020-12-17 04:19:01 -0500

    added some more tests

--- wl_rf_analysis/statistical_analysis.R
+++ wl_rf_analysis/statistical_analysis.R
@@ -7,7 +7,11 @@ install.packages("tibble")
 install.packages("rlist")
 install.packages("plot3D")
 install.packages("plotly")
-
+install.packages("MASS")
+install.packages("lme4")
+install.packages("car")
+install.packages("onewaytests")
+ 
 library(tibble)
 library(janitor)
 library(ggpubr)
@@ -16,6 +20,10 @@ library(openxlsx)
 library(rlist)
 library(plot3D)
 library(plotly)
+library(MASS)
+library(lme4)
+library(car)
+library(onewaytests)
 
 setwd("D:/onedrive/rpi/20fall/ITWS-4350/project")
 getwd()
@@ -87,50 +95,123 @@ rain_data <- read.xlsx("chennai_reservoir_rainfall.xlsx", detectDates = TRUE)
 ###########
 f_POONDI_WL <- as.numeric(as.character(factor(water_level_data$POONDI)))
 f_POONDI_RF <- as.numeric(as.character(factor(rain_data$POONDI)))
-#hist(f_POONDI_RF, breaks = 300)
+#kruskal.test(f_POONDI_WL ~ f_POONDI_RF)
+#hist(f_POONDI_RF, breaks = 300, main="POONDI rainfall data not processed")
 tmp <- remove_drought_days(f_POONDI_WL, f_POONDI_RF)
 f_POONDI_WL <- as.numeric(as.character(factor(unlist(tmp["WL"]))))
 f_POONDI_RF <- as.numeric(as.character(factor(unlist(tmp["RF"]))))
-
+tmp.data <- data.frame(f_POONDI_WL, f_POONDI_RF)
+bf.test(f_POONDI_WL ~ as.factor(f_POONDI_RF), data=tmp.data, alpha = 0.05)
+#hist(f_POONDI_RF, breaks = 300, main="POONDI rainfall data processed")
 model1 <- aov(f_POONDI_WL ~ f_POONDI_RF)
+#leveneTest(as.numeric(as.character(factor(f_POONDI_WL))), factor(f_POONDI_RF))
+#fligner.test(f_POONDI_WL ~ f_POONDI_RF)
+model1.1 <- lm(f_POONDI_WL ~ 0 + f_POONDI_RF)
+
+#kruskal.test(f_POONDI_WL ~ f_POONDI_RF)
+
+#tmp.data <- data.frame(f_POONDI_WL, f_POONDI_RF)
+#bf.test(f_POONDI_WL ~ tmp.data, data=tmp.data, alpha = 0.05)
+
+#glmer(f_POONDI_WL ~ (1|f_POONDI_RF), family=poisson(link = "log"))
+#modelz<-glm(f_POONDI_WL~f_POONDI_RF,family=poisson)
+#summary(modelz) 
+#plot(modelz)
+
+#bc <- boxcox(model1.1, lambda = seq(-3,5))
+#best.lam = bc$x[which(bc$y==max(bc$y))]
+#modelz <- lm((f_POONDI_WL ^ 0.1) ~ f_POONDI_RF)
+#plot(modelz)
+#hist(residuals(modelz))
+
+#model1.2 <- aov(f_POONDI_WL ~ 1/f_POONDI_RF)
+#summary(model1)
+#summary(model1.2)
+
+#model1.1 <- lm(f_POONDI_WL ~ 0 + 1/f_POONDI_RF)
+#plot(residuals(model1.1))
+#summary(model1.1)
+
+#ggqqplot(residuals(model1))
+#shapiro_test(residuals(model1.1))
+#plot(f_POONDI_RF, resid(model1.1))
+#plot(residuals(model1.1), fitted(model1.1))
+
+
 #shapiro_test(sample(residuals(model1), 5000))
 #plot(model1)
-#ggqqplot(residuals(model1))
+ggqqplot(residuals(model1))
 
 #CHOLAVARAM
 ###########
 f_CHOLAVARAM_WL <- as.numeric(as.character(factor(water_level_data$CHOLAVARAM)))
 f_CHOLAVARAM_RF <- as.numeric(as.character(factor(rain_data$CHOLAVARAM)))
-#hist(f_CHOLAVARAM_RF, breaks = 300)
+#kruskal.test(f_CHOLAVARAM_WL ~ f_CHOLAVARAM_RF)
+#hist(f_CHOLAVARAM_RF, breaks = 300, main="CHOLAVARAM rainfall data not processed")
 tmp <- remove_drought_days(f_CHOLAVARAM_WL, f_CHOLAVARAM_RF)
 f_CHOLAVARAM_WL <- as.numeric(as.character(factor(unlist(tmp["WL"]))))
 f_CHOLAVARAM_RF <- as.numeric(as.character(factor(unlist(tmp["RF"]))))
+#hist(f_CHOLAVARAM_RF, breaks = 300, main="CHOLAVARAM rainfall data processed")
 model2 <- aov(f_CHOLAVARAM_WL ~ f_CHOLAVARAM_RF)
+model2.1 <- lm(f_CHOLAVARAM_WL ~ 0 + f_CHOLAVARAM_RF)
+#leveneTest(as.numeric(as.character(factor(f_CHOLAVARAM_WL))), factor(f_CHOLAVARAM_RF))
+#fligner.test(f_CHOLAVARAM_WL ~ f_CHOLAVARAM_RF)
+#kruskal.test(f_CHOLAVARAM_WL ~ f_CHOLAVARAM_RF)
 
 #REDHILLS
 ###########
 f_REDHILLS_WL <- as.numeric(as.character(factor(water_level_data$REDHILLS)))
 f_REDHILLS_RF <- as.numeric(as.character(factor(rain_data$REDHILLS)))
-#hist(f_REDHILLS_RF, breaks = 300)
+#kruskal.test(f_REDHILLS_WL ~ f_REDHILLS_RF)
+#hist(f_REDHILLS_RF, breaks = 300, main="REDHILLS rainfall data not processed")
 tmp <- remove_drought_days(f_REDHILLS_WL, f_REDHILLS_RF)
 f_REDHILLS_WL <- as.numeric(as.character(factor(unlist(tmp["WL"]))))
 f_REDHILLS_RF <- as.numeric(as.character(factor(unlist(tmp["RF"]))))
+#hist(f_REDHILLS_RF, breaks = 300, main="REDHILLS rainfall data processed")
 model3 <- aov(f_REDHILLS_WL ~ f_REDHILLS_RF)
+#leveneTest(as.numeric(as.character(factor(f_REDHILLS_WL))), factor(f_REDHILLS_RF))
+#fligner.test(f_REDHILLS_WL ~ f_REDHILLS_RF)
+
+model3.1 <- lm(f_REDHILLS_WL ~ 0 + f_REDHILLS_RF)
+
+#kruskal.test(f_REDHILLS_WL ~ f_REDHILLS_RF)
 
 #CHEMBARAMBAKKAM
 ###########
 f_CHEMBARAMBAKKAM_WL <- as.numeric(as.character(factor(water_level_data$CHEMBARAMBAKKAM)))
 f_CHEMBARAMBAKKAM_RF <- as.numeric(as.character(factor(rain_data$CHEMBARAMBAKKAM)))
-#hist(f_CHEMBARAMBAKKAM_RF, breaks = 300)
+#kruskal.test(f_CHEMBARAMBAKKAM_WL ~ f_CHEMBARAMBAKKAM_RF)
+#hist(f_CHEMBARAMBAKKAM_RF, breaks = 300, main="CHEMBARAMBAKKAM rainfall data not processed")
 tmp <- remove_drought_days(f_CHEMBARAMBAKKAM_WL, f_CHEMBARAMBAKKAM_RF)
 f_CHEMBARAMBAKKAM_WL <- as.numeric(as.character(factor(unlist(tmp["WL"]))))
 f_CHEMBARAMBAKKAM_RF <- as.numeric(as.character(factor(unlist(tmp["RF"]))))
+#hist(f_CHEMBARAMBAKKAM_RF, breaks = 300, main="CHEMBARAMBAKKAM rainfall data processed")
 model4 <- aov(f_CHEMBARAMBAKKAM_WL ~ f_CHEMBARAMBAKKAM_RF)
+
+#leveneTest(as.numeric(as.character(factor(f_CHEMBARAMBAKKAM_WL))), factor(f_CHEMBARAMBAKKAM_RF))
+#fligner.test(f_CHEMBARAMBAKKAM_WL ~ f_CHEMBARAMBAKKAM_RF)
+
+model4.1 <- lm(f_CHEMBARAMBAKKAM_WL ~ 0 + f_CHEMBARAMBAKKAM_RF)
+
+#kruskal.test(f_CHEMBARAMBAKKAM_WL ~ f_CHEMBARAMBAKKAM_RF)
 
 summary(model1)
 summary(model2)
 summary(model3)
 summary(model4)
+
+summary(model1.1)
+summary(model2.1)
+summary(model3.1)
+summary(model4.1)
+
+plot(model1.1)
+plot(model2.1)
+plot(model3.1)
+plot(model4.1)
+
+
+plot(residuals(model2), fitted(model2))
 
 #shapiro_test(residuals(model1))
 #shapiro_test(residuals(model2))
@@ -142,10 +223,10 @@ hist(residuals(model2))
 hist(residuals(model3))
 hist(residuals(model4))
 
-ggqqplot(residuals(model1))
-ggqqplot(residuals(model2))
-ggqqplot(residuals(model3))
-ggqqplot(residuals(model4))
+ggqqplot(residuals(model1), main="POONDI res Normal Q")
+ggqqplot(residuals(model2), main="CHOLAVARAM res Normal Q")
+ggqqplot(residuals(model3), main="REDHILLS res Normal Q")
+ggqqplot(residuals(model4), main="CHEMBARAMBAKKAM res Normal Q")
 
 
 re_list <- list("POONDI" = residuals(model1), "CHOLAVARAM" = residuals(model2), "REDHILLS" = residuals(model3), "CHEMBARAMBAKKAM" = residuals(model4))
